65e86c21cc335072d58dca93340b4e4aaf2c2f87
Adam Argyle <atom@argyleink.com>
2019-06-27 15:06:24 -0700

    syntax highlighting, deep links, ios fixes

--- app/index.js
+++ app/index.js
@@ -1,6 +1,3 @@
-// import Whatever from 'https://unpkg.com/whatever'
-// import Whatever from 'whatever'
-
-import { default as index_css }  from './index.css'
-
-console.info(index_css)
+import 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.16.0/prism.min.js'
+import 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.16.0/plugins/line-numbers/prism-line-numbers.min.js'
+import 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.16.0/plugins/normalize-whitespace/prism-normalize-whitespace.min.js'
