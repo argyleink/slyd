27afcd72dccafd0cf1a486fd70dbfa7aa6281938
Adam Argyle <atom@argyleink.com>
2019-07-06 22:14:25 -0700

    check for intersection observer support before using

--- app/slyd/slyd-observer.js
+++ app/slyd/slyd-observer.js
@@ -5,19 +5,21 @@ const state = {
 export const onSlydChanged = cb =>
   state.callbacks.push(cb)
 
-const slyd_observer = new IntersectionObserver(slides => {
-  const [slyd] = slides.filter(slide => slide.isIntersecting)
-  
-  if (slyd)
-    state.callbacks.forEach(cb =>
-      cb(slyd))
-}, {
-  root: document.querySelector('main'),
-  rootMargin: '0px',
-  threshold: 1,
-})
+if (window.hasOwnProperty('IntersectionObserver')) {
+  const slyd_observer = new IntersectionObserver(slides => {
+    const [slyd] = slides.filter(slide => slide.isIntersecting)
+    
+    if (slyd)
+      state.callbacks.forEach(cb =>
+        cb(slyd))
+  }, {
+    root: document.querySelector('main'),
+    rootMargin: '0px',
+    threshold: 1,
+  })
 
-Array.from(document.querySelectorAll('slyd'))
-  .filter((slyd, i) => i !== 0)
-  .forEach(slyd => 
-    slyd_observer.observe(slyd))
+  Array.from(document.querySelectorAll('slyd'))
+    .filter((slyd, i) => i !== 0)
+    .forEach(slyd => 
+      slyd_observer.observe(slyd))
+}
