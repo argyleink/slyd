1308c9314a0dc477e299f6416d3feba51afc540e
Adam Argyle <atom@argyleink.com>
2019-07-21 22:31:00 -0700

    change active slide to slyd-active

--- app/slyd/slyd-manager.js
+++ app/slyd/slyd-manager.js
@@ -12,7 +12,7 @@ const state = {
 
 const setActive = ({target:in_view_slide}) => {
   state.slyds.active = in_view_slide
-  state.slyds.active.setAttribute('active', true)
+  state.slyds.active.setAttribute('slyd-active', true)
   in_view_slide.setAttribute('tabIndex', -1)
   setSrcs(in_view_slide)
   playVideo(in_view_slide)
@@ -22,7 +22,7 @@ const removeActive = () => {
   const {active} = state.slyds
 
   if (active) {
-    active.removeAttribute('active')
+    active.removeAttribute('slyd-active')
     active.removeAttribute('tabIndex')
     pauseVideo(active)
   }
