f36e1284b9ff42419d44f9eb88cfaa6de35ac5d8
Adam Argyle <atom@argyleink.com>
2019-07-29 22:36:16 -0700

    output to dist

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,14 @@ import postcss from 'rollup-plugin-postcss'
 
 import { default as importHTTP } from 'import-http/rollup'
 
+const is_prod = process.env.NODE_ENV
+  ? true
+  : false
+
 export default {
   input: 'app/index.js',
   output: {
-    file: 'app/slyd.js',
+    file: is_prod ? 'dist/slyd.js' : 'app/slyd.js',
     format: 'es',
   },
   plugins: [
