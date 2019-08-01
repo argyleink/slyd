163cba889d412d02e1391078483704aba0451675
Adam Argyle <atom@argyleink.com>
2019-07-31 22:25:34 -0700

    minify css

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,31 @@
 const postcssPresetEnv  = require('postcss-preset-env')
 const postcssImport     = require('postcss-import')
 const postcsseasings    = require('postcss-easings')
+const postcssnano       = require('cssnano')
+
+const is_prod = process.env.NODE_ENV
+  ? true
+  : false
+
+const dev_plugins = [
+  postcsseasings(),
+  postcssImport(),
+  postcssPresetEnv({
+    stage: 0,
+    browsers: [
+      '>0.25%',
+      'not ie 11',
+      'not op_mini all',
+    ],
+  }),
+]
+
+const prod_plugins = [
+  postcssnano({preset: 'default'}),
+]
 
 module.exports = {
-  plugins: [
-    postcsseasings(),
-    postcssImport(),
-    postcssPresetEnv({
-      stage: 0,
-      browsers: [
-        '>0.25%',
-        'not ie 11',
-        'not op_mini all',
-      ],
-    }),
-  ]
+  plugins: is_prod
+    ? [...dev_plugins, ...prod_plugins]
+    : dev_plugins
 }
