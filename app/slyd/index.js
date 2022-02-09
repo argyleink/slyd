b0273c1d75dbc4e4e9f8f8a6ec039e3932a943bb
Adam Argyle <atom@argyleink.com>
2022-02-08 22:24:28 -0800

    update prism

--- app/slyd/index.js
+++ app/slyd/index.js
@@ -1,6 +1,6 @@
-import 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.16.0/prism.min.js'
-import 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.16.0/plugins/line-numbers/prism-line-numbers.min.js'
-import 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.16.0/plugins/normalize-whitespace/prism-normalize-whitespace.min.js'
+import 'https://unpkg.com/prismjs'
+import 'https://unpkg.com/prismjs@1.26.0/plugins/line-numbers/prism-line-numbers.min.js'
+import 'https://unpkg.com/prismjs@1.26.0/plugins/normalize-whitespace/prism-normalize-whitespace.min.js'
 import 'https://polyfill.io/v3/polyfill.min.js?features=IntersectionObserver%2CSet'
 
 import './slyd-manager.js'
